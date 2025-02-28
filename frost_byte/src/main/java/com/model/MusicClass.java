dc0119e867ffa29914bbf606577056aa09de414b
QuietKat329 <kgturner0909@gmail.com>
2025-02-28 12:34:20 -0500

    added user files

---
+++ frost_byte/src/main/java/com/model/MusicClass.java
@@ -0,0 +1,15 @@
+package com.model;
+
+import java.util.ArrayList;
+
+public class MusicClass {
+    private ArrayList<Student> musicClass;
+    private String musClassCode;
+    private ArrayList<Lesson> assignments;
+    private String makeClassCode(){
+        return " ";
+    }
+    private void deleteClass(String musClassCode){
+
+    }
+}
---
+++ frost_byte/src/main/java/com/model/UserList.java
@@ -0,0 +1,22 @@
+package com.model;
+import java.util.ArrayList;
+
+public class UserList {
+    private ArrayList<User> users;
+    private UserList userList;
+    private UserList(){
+
+    }
+    public UserList getIntance(){
+        return userList;
+    }
+    public void addUser(String firstName, String lastName){
+
+    }
+    public User getUser(String UserName){
+        return userList.getUser(UserName);
+    }
+    public void saveUser(){
+        
+    }
+}
