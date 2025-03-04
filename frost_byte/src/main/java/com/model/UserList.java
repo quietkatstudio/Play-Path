687a79f8fed0571eca89c02d7fa274f39803cf9c
QuietKat329 <kgturner0909@gmail.com>
2025-03-04 10:46:53 -0500

    updated the user and userlist class

--- frost_byte/src/main/java/com/model/UserList.java
+++ frost_byte/src/main/java/com/model/UserList.java
@@ -1,44 +1,76 @@
 package com.model;
 
 import java.util.ArrayList;
-
+/**
+ * This is a singleton class that manages the list of users 
+ * @author 
+ */
 public class UserList {
 
     private static UserList userList = new UserList();
     private ArrayList<User> users = new ArrayList<>();
 
+    /**
+     * This method constructs the users list using the user json file
+     */
     private UserList() {
         users = DataLoader.getUsers();
+        if(users == null){
+            users = new ArrayList<>();
+        }
     }
 
+    /**
+     * Returns the singleton instance of userlist
+     * @return the userlist instance
+     */
     public static UserList getInstance() {
+        if (userList == null){
+            userList = new UserList();
+        }
         return userList;
     }
 
-    public void addUser(String firstName, String lastName) {
+    /**
+     * This method adds a new user to the Array List UserList
+     * @param userName the users username
+     * @param firstName the users first name 
+     * @param lastName the users last name
+     * @param email the users email
+     * @param password the users password
+     */
+    public void addUser(String userName, String firstName, String lastName, String email, String password) {
+        User newUser = new User(userName, firstName, lastName, email, false, password);
+        users.add(newUser);
+        saveUsers();
 
     }
 
     /**
-     * This method iterates through the userlist and returns the User with the
-     * matching UserName
-     * 
-     * @param UserName
-     * @return
+     * This method iterates through the userlist and returns the User with the matching UserName
+     * @param UserName the users username
+     * @return the user
      */
     public User getUser(String UserName) {
         for (User user : users) {
-            if (user.getUserName().equals(UserName)) {
+            if (user.getUserName().equalsIgnoreCase(UserName)) {
                 return user;
             }
         }
         return null;
     }
 
-    public void saveUser() {
+    /**
+     * This method saves the user list to the data writer
+     */
+    public void saveUsers() {
         DataWriter.saveUsers();
     }
 
+    /**
+     * This method gets the full list of users
+     * @return the users
+     */
     public ArrayList<User> getUsers() {
         return users;
     }
