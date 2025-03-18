e0b91d6e7421f455f9159811240b36281b23d5cb
QuietKat329 <kgturner0909@gmail.com>
2025-03-18 15:57:45 -0400

    changed the lesson file and lesson json file

--- frost_byte/src/main/java/com/model/Lesson.java
+++ frost_byte/src/main/java/com/model/Lesson.java
@@ -1,5 +1,27 @@
 package com.model;
+import java.util.UUID;
 
 public class Lesson {
-    
-}
+    private UUID id;
+    private String title;
+    private String description;
+    private String content;
+    private Song song;
+//      private UUID id;
+//     private String userName;
+//     private String firstName;
+//     private String lastName;
+//     private String email;
+//     private String password;
+//     private boolean isTeacher;
+
+//     public User(String userName, String firstName, String lastName, String email, String password, boolean isTeacher) {
+//         this.id = UUID.randomUUID();
+//         this.userName = userName;
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.email = email;
+//         this.isTeacher = isTeacher;
+//         this.password = password;
+//     }
+ }
