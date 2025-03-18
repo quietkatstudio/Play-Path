7bfce599dac1477ffa71c0ee8dada193cacaf28c
QuietKat329 <kgturner0909@gmail.com>
2025-03-18 16:24:59 -0400

    updated the lesson.java file

--- frost_byte/src/main/java/com/model/Lesson.java
+++ frost_byte/src/main/java/com/model/Lesson.java
@@ -1,27 +1,43 @@
 package com.model;
+import java.util.ArrayList;
 import java.util.UUID;
+
+import com.Flashcard;
 
 public class Lesson {
     private UUID id;
     private String title;
     private String description;
     private String content;
+    private ArrayList<Flashcard> flashcards;
+    //private ArrayList<Quiz> quiz;
     private Song song;
-//      private UUID id;
-//     private String userName;
-//     private String firstName;
-//     private String lastName;
-//     private String email;
-//     private String password;
-//     private boolean isTeacher;
-
-//     public User(String userName, String firstName, String lastName, String email, String password, boolean isTeacher) {
-//         this.id = UUID.randomUUID();
-//         this.userName = userName;
-//         this.firstName = firstName;
-//         this.lastName = lastName;
-//         this.email = email;
-//         this.isTeacher = isTeacher;
-//         this.password = password;
-//     }
+    public Lesson(UUID id, String title, String description, String content, ArrayList<Flashcard> flashcards, Song song){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.content = content;
+        
+    }
+    public UUID getID(){
+        return id;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public String getContent(){
+        return content;
+    }
+    public ArrayList<Flashcard> getFlashcards(){
+        return flashcards;
+    }
+    // public Quiz getQuiz(){
+    //     return quiz;
+    // }
+    public Song getSong(){
+        return song;
+    }
  }
