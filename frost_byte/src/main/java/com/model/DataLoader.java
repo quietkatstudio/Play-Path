e26ce2d66598fa021ecd4ed04280d35ad34dc478
JP-Grimes <grimesjp@email.sc.edu>
2025-03-20 12:43:50 -0400

    Added GetSong, GetLesson, and modified User

--- frost_byte/src/main/java/com/model/DataLoader.java
+++ frost_byte/src/main/java/com/model/DataLoader.java
@@ -1,6 +1,7 @@
 package com.model;
 
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.UUID;
 
@@ -42,28 +43,111 @@ public class DataLoader extends DataConstants {
         return users;
     }
 
-    public static ArrayList<Lesson> getLessons(){
+    public static ArrayList<Lesson> getLessons() {
         ArrayList<Lesson> lessons = new ArrayList<Lesson>();
+
+        try {
+            FileReader reader = new FileReader("json\\lessons.json");
+            JSONParser parser = new JSONParser();
+            JSONArray lessonsJson = (JSONArray) parser.parse(reader);
+
+            for (int i = 0; i < lessonsJson.size(); i++) {
+                JSONObject lessonJSON = (JSONObject) lessonsJson.get(i);
+                lessons.add(parseLessonJSON(lessonJSON));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return lessons;
     }
 
-
-
-    public static ArrayList<Song> getSongs(){
+    public static ArrayList<Song> getSongs() {
         ArrayList<Song> songs = new ArrayList<Song>();
+
+        try {
+            FileReader reader = new FileReader("json\\songs.json");
+            JSONParser parser = new JSONParser();
+            JSONArray songsJson = (JSONArray) parser.parse(reader);
+
+            for (int i = 0; i < songsJson.size(); i++) {
+                JSONObject songJSON = (JSONObject) songsJson.get(i);
+                songs.add(parseSongJSON(songJSON));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return songs;
     }
 
+    private static Lesson parseLessonJSON(JSONObject jsonLesson) {
+        UUID id = UUID.fromString((String) jsonLesson.get("id"));
+        String title = (String) jsonLesson.get("title");
+        String description = (String) jsonLesson.get("description");
+        String content = (String) jsonLesson.get("content");
 
+        ArrayList<Flashcard> flashcards = new ArrayList<>();
+        JSONArray flashcardsArray = (JSONArray) jsonLesson.get("flashcards");
+        for (int i = 0; i < flashcardsArray.size(); i++) {
+            JSONObject flashcardJSON = (JSONObject) flashcardsArray.get(i);
+            String term = (String) flashcardJSON.get("term");
+            String definition = (String) flashcardJSON.get("definition");
+            flashcards.add(new Flashcard(term, definition));
+        }
 
+        JSONObject quizJSON = (JSONObject) jsonLesson.get("quiz");
+        String question = (String) quizJSON.get("question");
+        JSONArray optionsArray = (JSONArray) quizJSON.get("options");
+        ArrayList<String> options = new ArrayList<>();
+        for (Object option : optionsArray) {
+            options.add(option.toString());
+        }
+        String answer = (String) quizJSON.get("answer");
+        Quiz quiz = new Quiz(question, options, answer);
 
+        JSONObject songJSON = (JSONObject) jsonLesson.get("song");
+        Song song = parseSongJSON(songJSON);
 
+        return new Lesson(id, title, description, content, flashcards, quiz, song);
+    }
 
+    private static Song parseSongJSON(JSONObject jsonSong) {
+        String title = (String) jsonSong.get("title");
+        String author = (String) jsonSong.get("author");
+        String genre = (String) jsonSong.get("genre");
+        String duration = (String) jsonSong.get("duration");
+        String tempo = (String) jsonSong.get("tempo");
+        int defTimeSigNumer = ((Long) jsonSong.get("defTimeSigNumer")).intValue();
+        int defTimeSigDenom = ((Long) jsonSong.get("defTimeSigDenom")).intValue();
+        JSONObject defKeySig = (JSONObject) jsonSong.get("defTimeSigDenom");
+
+        JSONObject quizJSON = (JSONObject) jsonSong.get("quiz");
+        String question = (String) quizJSON.get("question");
+        JSONArray optionsArray = (JSONArray) quizJSON.get("options");
+
+        JSONObject measureJson = (JSONObject) jsonSong.get("quiz");
+        JSONArray measuresArray = (JSONArray) jsonSong.get("measures");
+        ArrayList<Measure> measuresArrayList = new ArrayList<>();
+        for (Object measure : measuresArray) {
+            measuresArrayList.add(new measure());
+        }
+
+        return new Song(title, author, genre, duration, tempo, defTimeSigNumer, defTimeSigDenom, defKeySig,
+                measuresArrayList);
+
+    }
+
+    // DataLoader Tests
     public static void main(String[] args) {
         ArrayList<User> users = DataLoader.getUsers();
 
         for (User user : users) {
             System.out.println(user);
+        }
+
+        System.out.println("Loading songs >>>");
+        ArrayList<Song> songs = DataLoader.getSongs();
+        for (Song song : songs) {
+            System.out.println(song);
         }
     }
 
