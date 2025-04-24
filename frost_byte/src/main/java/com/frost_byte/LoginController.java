902f534bda02f3f2ea2768be7a23f17e97833283
JP-Grimes <grimesjp@email.sc.edu>
2025-04-24 12:22:55 -0400

    a

---
+++ frost_byte/src/main/java/com/frost_byte/LoginController.java
@@ -0,0 +1,14 @@
+package com.frost_byte;
+
+public class LoginController {
+
+    private PrimaryController primary;
+
+    public void setPrimaryController(PrimaryController controller) {
+        this.primary = controller;
+
+    }
+
+    // add logic here
+
+}
